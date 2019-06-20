1e2061b3bdbc64080a8ba08f5b8f7c9d5cf6fc8c
raylison nunes dos santos <raylison100@gmail.com>
2019-06-20 15:39:13 -0300

    Ajuste rota confirmação de senha

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -38,7 +38,7 @@ class PasswordResetRequest extends Notification implements ShouldQueue
      */
     public function toMail($notifiable)
     {
-        $url = url('/api/betterworld/activate/'.$this->token);
+        $url = url('/api/betterworld/password/find/'.$this->token);
 //        $url = url('http://localhost:4200/#/auth/confirm-account/'.$this->token);
         return (new MailMessage)
             ->line('You are receiving this email because we        received a password reset request for your account.')
